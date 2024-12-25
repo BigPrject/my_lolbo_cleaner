eac9c8477dbf6e3e78e1c58e99ee69f73d0d142d
BigPrject <sytro46@gmail.com>
2024-12-25 01:04:09 +0000

    added spectral norm

--- lolbo/utils/bo_utils/base.py
+++ lolbo/utils/bo_utils/base.py
@@ -12,18 +12,23 @@ class Swish(torch.nn.Module):
 
 
 class _LinearBlock(torch.nn.Sequential):
-    def __init__(self, input_dim, output_dim, swish):
+    def __init__(self, input_dim, output_dim, swish,spectral_norm=False):
+        layers = OrderedDict()
+        
+        # Fully connected layer
+        fc = torch.nn.Linear(input_dim, output_dim)
+        if spectral_norm:
+            fc = torch.nn.utils.spectral_norm(fc)  # Apply spectral normalization
+        layers["fc"] = fc
+        
         if swish:
-            super().__init__(OrderedDict([
-                ("fc", torch.nn.Linear(input_dim, output_dim)),
-                ("swish", Swish()),
-            ]))
+            layers["swish"] = Swish()
         else:
-            super().__init__(OrderedDict([
-                ("fc", torch.nn.Linear(input_dim, output_dim)),
-                ("norm", torch.nn.BatchNorm1d(output_dim)),
-                ("relu", torch.nn.ReLU(True)),
-            ]))
+            layers["norm"] = torch.nn.BatchNorm1d(output_dim)
+            layers["relu"] = torch.nn.ReLU(inplace=True)
+        
+        # init layers
+        super().__init__(layers)
 
 
 class DenseNetwork(torch.nn.Sequential):
@@ -35,7 +40,7 @@ class DenseNetwork(torch.nn.Sequential):
         else:
             prev_dims = [input_dim] + list(hidden_dims[:-1])
             layers = OrderedDict([
-                (f"hidden{i + 1}", _LinearBlock(prev_dim, current_dim, swish=swish))
+                (f"hidden{i + 1}", _LinearBlock(prev_dim, current_dim, swish=swish,spectral_norm=True))
                 for i, (prev_dim, current_dim) in enumerate(zip(prev_dims, hidden_dims))
             ])
             self.output_dim = hidden_dims[-1]
